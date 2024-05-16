3c8abc1f9d3a5936c2dc4516ac136a4cc230d3c0
june <june.korbiz@gmail.com>
2024-05-17 00:46:19 +0900

    feat background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,60 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import "./App.css";
 
 const App: React.FC = () => {
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const handleClick = (index: number) => {
+        setActiveIndex(activeIndex === index ? null : index);
+    };
+
     return (
         <div className="container">
-            <motion.div className="box" whileHover={{ scale: 1.1 }} />
-            <motion.div className="box" whileHover={{ scale: 1.1 }} />
-            <motion.div className="box" whileHover={{ scale: 1.1 }} />
-            <motion.div className="box" whileHover={{ scale: 1.1 }} />
+            {[0, 1, 2, 3].map((index) => (
+                <div key={index}>
+                    {activeIndex === index && (
+                        <AnimatePresence>
+                            <motion.div
+                                className="box"
+                                onClick={() => handleClick(index)}
+                                initial={{
+                                    scale: 1,
+                                    opacity: 0.5,
+                                    x: index % 2 === 0 ? "-30%" : "10%",
+                                    y: index < 2 ? "-20%" : "20%",
+                                }}
+                                animate={{
+                                    scale: 1.5,
+                                    x: "-50%",
+                                    y: "-50%",
+                                    opacity: 1,
+                                }}
+                                exit={{}}
+                                style={{
+                                    position: "fixed",
+                                    top: "50%",
+                                    left: "50%",
+                                    zIndex: 10,
+                                }}
+                                key={index}
+                                transition={{ duration: 0.3 }}
+                            />
+                        </AnimatePresence>
+                    )}
+                    {activeIndex !== index && (
+                        <motion.div
+                            className="box"
+                            onClick={() => handleClick(index)}
+                            whileHover={{ scale: 1.1 }}
+                            animate={{
+                                opacity: 0.5,
+                            }}
+                            key={index}
+                        />
+                    )}
+                </div>
+            ))}
         </div>
     );
 };
