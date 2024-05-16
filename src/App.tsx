ba629462038d3f9aa068e39032242409fc81b4a8
june <june.korbiz@gmail.com>
2024-05-17 00:20:56 +0900

    add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <div className="container">
+            <motion.div className="box" whileHover={{ scale: 1.1 }} />
+            <motion.div className="box" whileHover={{ scale: 1.1 }} />
+            <motion.div className="box" whileHover={{ scale: 1.1 }} />
+            <motion.div className="box" whileHover={{ scale: 1.1 }} />
+        </div>
+    );
+};
 
 export default App;
